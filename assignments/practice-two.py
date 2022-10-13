4ab370dd99e8841ea706a712cfc027d91f5e15fc
TheTeaCup <lw0482352@gmail.com>
2022-10-12 22:42:43 -0400

    Uploading first two assignments

--- assignments/practice-two.py
+++ assignments/practice-two.py
@@ -1,1 +1,7 @@
 # Sales Prediction
+
+# Get user input
+sales = float(input('Enter your projected total sales: '))
+
+# Inform user on expected profits
+print('Your expected profits are: ', sales * 0.23)
--- assignments/practive-six.py
+++ assignments/practive-six.py
@@ -1,1 +1,17 @@
 # Sales Tax Program
+
+# How much was the item
+purchaseAmount = float(input('Please enter how much your purchase was: '))
+
+# How many payments to pay off
+payOffTime = float(input('How many payments do you wish to make: '))
+
+# Calculate total with interest
+totalAmount = (0.05 * purchaseAmount) + purchaseAmount
+
+# Calculate the amount added per payment
+payOffCost = totalAmount / payOffTime
+
+# Print the total amount with interest and the amount per payment added
+print("The total purchase amount with interest is: ", totalAmount)
+print("The amount added each payment is: ", payOffCost)
