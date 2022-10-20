ee4f795ab1661c361474d8523f3920ac7c56e217
TheTeaCup <lw0482352@gmail.com>
2022-10-19 21:43:00 -0400

    added: lists.py

---
+++ book-work/lists.py
@@ -0,0 +1,15 @@
+# Examples of different lists
+
+# Example one
+first = list(range(1, 10, 2))
+print(first)
+
+# Example two
+second = [0] * 5
+print(second)
+
+# Example three
+third = [1, 2, 3] * 3
+print(third)
+
+
