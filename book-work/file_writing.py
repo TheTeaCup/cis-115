d5f70b2a8ae81ef0bde9b3ebb350db00cbe592e4
TheTeaCup <lw0482352@gmail.com>
2022-10-19 21:23:22 -0400

    add: file_writing.py

---
+++ book-work/file_writing.py
@@ -0,0 +1,18 @@
+# This file has an example of writing data to a file
+
+def writeFile():
+    num_days = int(input('How many days do you have sales for: '))
+
+    sales_file = open('../data/sales.txt', 'w')
+    for count in range(1, num_days + 1):
+        sales = float(input('Enter the sales for day #' + str(count) + ': '))
+
+        # write to file
+        sales_file.write(str(sales) + '\n')
+
+    # close file
+    sales_file.close()
+    print('Data written to sales.txt')
+
+
+writeFile()  # call function
