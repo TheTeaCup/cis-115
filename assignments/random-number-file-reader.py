0245edf1fd4264d1a5a3d78a8da0e07ee829017a
TheTeaCup <lw0482352@gmail.com>
2022-11-09 12:55:35 -0500

    working on this week's assignments

--- assignments/random-number-file-reader.py
+++ assignments/random-number-file-reader.py
@@ -1,2 +1,6 @@
 # Random Number File Reader
 # Chapter 6 | Programming Exercise 8
+
+def read():
+    file = open('random-number-file.txt', 'r')
+
--- assignments/random-number-file-writer.py
+++ assignments/random-number-file-writer.py
@@ -1,2 +1,16 @@
 # Random Number File Writer
 # Chapter 6 | Programming Exercise 7
+
+def write():
+    file = open('random-number-file.txt', 'w')  # open file
+    number = int(input('Enter the number of words for this file: '))  # get user inout
+    response = ''
+
+    for i in range(number):  # take user input to make a quantity
+        response += input('Please provide a word: ') + ' '
+        print(response)
+        file.write(response)
+    file.close()
+
+
+write()  # call function
